b2f73d3a47a7e84dc912a3de3bb58bd8657b0d75
JeannetteD <Jeannettedariano@gmail.com>
2016-09-27 16:10:08 -0700

    pick the greater number

---
+++ greaterNum.js
@@ -0,0 +1,15 @@
+//What number's bigger?
+
+//Write a function named greaterNum that:
+
+//takes 2 arguments, both numbers.
+//returns whichever number is the greater (higher) number.
+//Test the function 2 times with different number pairs
+
+function greaterNum(int1, int2){
+  if (int1, int2){
+    alert(int1 + " is the greater number!")
+  } else{
+    alert(int2 + " is the greater number!")
+  }
+}
